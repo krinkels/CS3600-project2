80558a006f9674ccf7e9779386aa81cb88708e62
Wayne Lu <wayne.lu.8@gmail.com>
2014-10-14 20:43:01 -0400

    Improved extra credit

--- Extra/run_me.py
+++ Extra/run_me.py
@@ -4,6 +4,30 @@ import BinaryCSP
 lines = Testing.get_lines("cspAdjacency.csp")
 csp = Testing.csp_parse(lines)
 result = BinaryCSP.solve(csp)
-print "CSP modeling seating of guests in a row with constraints on specific seating"
-print "and adjacency to other guests. Results take the form of GUEST : SEAT"
-print result
+print "CSP modeling seating of guests in a row with constraints on specific seating and adjacency to other guests"
+print "Constraints are:"
+for constraint in csp.binaryConstraints:
+    if constraint.__class__ is BinaryCSP.AdjacentConstraint:
+        print " ", constraint.var1, "must be next to", constraint.var2
+    elif constraint.__class__ is BinaryCSP.NotAdjacentConstraint:
+        print " ", constraint.var1, "cannot be next to", constraint.var2
+for constraint in csp.unaryConstraints:
+    if constraint.__class__ is BinaryCSP.GoodValueConstraint:
+        print " ", constraint.var, "must be at seat", constraint.goodValue
+    elif constraint.__class__ is BinaryCSP.BadValueConstraint:
+        print " ", constraint.var, "cannot be at seat", constraint.badValue
+print
+print "CSP Solution"
+
+firstLine = "Seat:"
+secondLine = "Guest:"
+
+seats = {}
+for guest in result:
+    seats[result[guest]] = guest
+
+for seat in sorted(seats.keys()):
+    firstLine += "\t" + seat
+    secondLine += "\t" + seats[seat]
+print firstLine
+print secondLine
